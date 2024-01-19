8d28d886a05e29eec530a1be39dc318a4cc0ff34
julesss-coder <julesss@posteo.de>
2024-01-19 20:58:23 +0100

    Use module syntax

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,53 @@
+import { updateLocationWeather } from "./forecast.js";
+
 const locationForm = document.querySelector(".change-location");
 const weatherDetailsElement = document.querySelector(".details");
 const card = document.querySelector(".card");
 const timeOfDay = document.querySelector(".time");
 const weatherIcon = document.querySelector(".icon img");
 const errorElement = document.querySelector(".error");
+
+const displayErrorMessage = () => {
+  if (card.classList.contains("d-none") === false) {
+    card.classList.add("d-none");
+  }
+
+  if (errorElement.classList.contains("d-none")) {
+    errorElement.classList.remove("d-none");
+  }
+
+  errorElement.innerHTML = `<h5>The location you entered was not found.</h5>`;
+};
+
+const updateUI = (locationWeatherData) => {
+  const {locationDetails, locationWeather} = locationWeatherData;
+  
+  weatherDetailsElement.innerHTML = `<h5 class="city-name my-3">${locationDetails.EnglishName}</h5>
+    <h6 class="country-name my-3">${locationDetails.Country.EnglishName}</h6>
+    <div class="weather-condition my-3">${locationWeather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span class="temperature">${locationWeather.Temperature.Metric.Value} <span>&deg;C</span> | ${locationWeather.Temperature.Imperial.Value} <span>&deg;F</span></span>
+    </div>`;
+  
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+
+  if (errorElement.classList.contains("d-none") === false) {
+    errorElement.classList.add("d-none");
+  }
+
+  // Update timeOfDay icon
+  let timeOfDaySrc = null;
+  timeOfDaySrc = locationWeather.isDayTime ? '../img/day.svg' : '../img/night.svg';
+  timeOfDay.src = timeOfDaySrc;
+
+  // Update weather icon
+  let weatherIconSrc = null;
+  weatherIconSrc = `../img/icons/${locationWeather.WeatherIcon}.svg`;
+  weatherIcon.src = weatherIconSrc;
+
+};
 
 locationForm.addEventListener('submit', e => {
   e.preventDefault();
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -10,47 +10,8 @@ Separate UI code from API call code
 // Accuweather API key
 const key = 'xRGf1AI3AgaB1MvKh3QACiNGKpWMwIvR';
 
-const displayErrorMessage = () => {
-  if (card.classList.contains("d-none") === false) {
-    card.classList.add("d-none");
-  }
 
-  if (errorElement.classList.contains("d-none")) {
-    errorElement.classList.remove("d-none");
-  }
 
-  errorElement.innerHTML = `<h5>The location you entered was not found.</h5>`;
-};
-
-const updateUI = (locationWeatherData) => {
-  const {locationDetails, locationWeather} = locationWeatherData;
-
-  weatherDetailsElement.innerHTML = `<h5 class="city-name my-3">${locationDetails.EnglishName}</h5>
-    <h6 class="country-name my-3">${locationDetails.Country.EnglishName}</h6>
-    <div class="weather-condition my-3">${locationWeather.WeatherText}</div>
-    <div class="display-4 my-4">
-      <span class="temperature">${locationWeather.Temperature.Metric.Value} <span>&deg;C</span> | ${locationWeather.Temperature.Imperial.Value} <span>&deg;F</span></span>
-    </div>`;
-  
-  if (card.classList.contains("d-none")) {
-    card.classList.remove("d-none");
-  }
-
-  if (errorElement.classList.contains("d-none") === false) {
-    errorElement.classList.add("d-none");
-  }
-
-  // Update timeOfDay icon
-  let timeOfDaySrc = null;
-  timeOfDaySrc = locationWeather.isDayTime ? '../img/day.svg' : '../img/night.svg';
-  timeOfDay.src = timeOfDaySrc;
-
-  // Update weather icon
-  let weatherIconSrc = null;
-  weatherIconSrc = `../img/icons/${locationWeather.WeatherIcon}.svg`;
-  weatherIcon.src = weatherIconSrc;
-
-};
 
 const getLocationData = async (location) => {
   const baseURL = "http://dataservice.accuweather.com/locations/v1/cities/search";
@@ -79,6 +40,7 @@ const updateLocationWeather = async (location) => {
   };
 };
 
+export { updateLocationWeather };
 
 
 
